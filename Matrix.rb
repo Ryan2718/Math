9f427df5470d7d78dd68868c2e89ea01d3f94337
Ryan2718 <ryanmforsyth@gmail.com>
2015-02-07 22:19:29 -0500

    Update Matrix.rb
    Added two new methods:
    -copy
    -transpose
    
    Also added a newline character to print after the matrix is printed in the "display" method

--- Matrix.rb
+++ Matrix.rb
@@ -19,6 +19,31 @@ class Matrix
       end
       print "\n"
     end
+    print "\n"
+  end
+  
+  def copy
+    newValues = []
+    for row in 0...@m
+      newEntries = []
+      for col in 0...@n
+        newEntries.push(@values[row][col])
+      end
+      newValues.push(newEntries)
+    end
+    return Matrix.new(newValues)
+  end
+  
+  def transpose
+    rows = []
+    for col in 0...@n
+      entries = []
+      for row in 0...@m
+        entries.push(@values[row][col])
+      end
+      rows.push(entries)
+    end
+    return Matrix.new(rows)
   end
     
 end
