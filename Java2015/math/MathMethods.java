dc1744a1a2546120ba06b4d84af2b0632af44ec2
Ryan2718 <ryanmforsyth@gmail.com>
2015-02-16 19:07:30 -0500

    Create MathMethods.java
    A class with the single method of finding the greatest common divisor of two integers.

---
+++ Java2015/math/MathMethods.java
@@ -0,0 +1,28 @@
+package math;
+
+/** Static method for doing math.
+ * @author Ryan Forsyth (02/15/2015)
+ *
+ */
+public class MathMethods {
+
+	/** Determine the greatest common divisor of two positive numbers.
+	 * @param a The first number
+	 * @param b The second number
+	 * @return The greatest common divisors of the numbers
+	 */
+	public static int gcd(int a, int b) {
+		/* See Wikipedia page "Greatest Common Divisor" - 
+		 * "Using Euclid's Algorithm"
+		 */
+		if (a <= 0 || b <= 0) {
+			throw new RuntimeException("Parameters must be positive!");
+		} else if (a == b) {
+			return a;
+		} else if (a > b) {
+			return gcd(a - b, b);
+		} else { // b > a
+			return gcd(a, b - a);
+		}
+	}
+}
